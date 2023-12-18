28e1f8c647ce8a8d72909649d978bde4b2072012
janrainjerr <64010659@kmitl.ac.th>
2023-12-18 10:55:39 +0700

    lab-2 done

---
+++ lab-2/item-2/main.cpp
@@ -0,0 +1,24 @@
+/* Chapter : 2 - item : 2 - การแสดงข้อความ มากกว่า 1 บรรทัด ไม่ใช้ \n \t
+
+ให้เขียนโปรแกรมแสดงผลดังนี้
+- บรรทัดแรกสุด ด้านซ้ายมือ แสดงผล Computer
+- บรรทัดแรกสุด ด้านขวามือ แสดงผล Programming
+- บรรทัดสุุดท้าย (บรรทัดที่ 25) แสดงผลชิดขวา I am a hard-working student.
+- ห้ามใช้ \n \t
+
+หมายเหตุ
+
+1. การแสดงผลแต่ละบรรทัดมี 80 ตัวอักษร
+*/
+
+#include <stdio.h>
+int main()
+{
+	printf("%-40s","Computer");
+	printf("%40s","Programming");
+	printf("%1840s","");
+	printf("%-40s","");
+	printf("%40s","I am a hard-working student.");
+	
+	return 0;
+}
---
+++ lab-2/item-5/main.c
@@ -0,0 +1,26 @@
+/* Chapter : 2 - item : 5 - การแสดงข้อความ %s
+
+เติม พารามิเตอร์ตัวแรก (first parameter) ของ printf ให้เหมาะสม เพื่อแสดงผลตามตัวอย่าง
+
+int main() {
+
+  printf("1234567890123456789012345678901234567890\n");
+  printf("%  s\n","I am such a hardworking student.");
+  printf("%   s\n","I am such a hardworking student.");
+  printf("%  s\n","I am such a hardworking student.");
+  printf("%  s\n","I am such a hardworking student.");
+  printf("%   s\n","I am such a hardworking student.");
+
+  return 0;
+}
+*/
+int main() {
+  printf("1234567890123456789012345678901234567890\n");
+  printf("%40.9s\n","I am such a hardworking student.");
+  printf("%40.23s\n","I am such a hardworking student.");
+  printf("%40s\n","I am such a hardworking student.");
+  printf("%-40s\n","I am such a hardworking student.");
+  printf("%37s\n","I am such a hardworking student.");
+
+  return 0;
+}
