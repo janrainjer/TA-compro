3c02cfb4c184ff14ac2f61927428d3ca3565f897
phattharaphorn <64010659@kmitl.ac.th>
2023-12-19 19:45:21 +0700

    delete code

--- lab-04/item-1/main.cpp
+++ lab-04/item-1/main.cpp
@@ -2,22 +2,3 @@
 
 - รับจำนวนเต็ม 1 จำนวน แล้วตรวจสอบว่า หารด้วย 3 ลงตัวหรือไม่
 */
-
-#include <stdio.h>
-
-int main() {
-    unsigned long number;
-    printf(" *** Display integer in different styles ***\n");
-    printf("Enter an integer : ");
-    scanf("%lu", &number);
-    printf("Your number : %lu\n", number);
-    printf("variable size = %lu bytes\n", sizeof(number));
-    printf("last 3 digits : %03lu\n", number % 1000);
-    printf("next 3 digits : %03lu\n", (number / 1000) % 1000);
-    printf("next 3 digits : %03lu\n", (number / 1000000) % 1000);
-    printf("next 3 digits : %lu\n", number / 1000000000);
-    
-    printf("comma format  : %'lu\n", number);
-
-    return 0;
-}
